9322195c0e079dd7438e816f0f3429e39661abc8
Fury <furqode@gmail.com>
2021-12-22 21:26:20 +0500

    refactor code

--- script.js
+++ script.js
@@ -9,20 +9,30 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
-button.addEventListener("click", function() {
-    if (input.value.length > 0) {
-        var li = document.createElement("li");
+
+function inputLength() {
+    return input.value.length;
+}
+
+function createListElement() {
+    var li = document.createElement("li");
         li.appendChild(document.createTextNode(input.value));
         ul.appendChild(li);
         input.value="";
+}
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createListElement();
     }
-})
-input.addEventListener("keypress", function() {
-    if (input.value.length > 0 && event.keyCode === 13) {
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(input.value));
-        ul.appendChild(li);
-        input.value="";
+}
+
+function addListAfterKeypress(event) {
+    if (inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
     }
-})
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
 
