eb2d6c082573ee457560c5f714ac00f7351ff5da
Fury <furqode@gmail.com>
2021-12-23 17:16:40 +0500

    making a del button

--- script.js
+++ script.js
@@ -38,6 +38,10 @@ function addListAfterKeypress(event) {
     }
 }
 
+function del() {
+    
+}
+
 button.addEventListener("click", addListAfterClick);
 input.addEventListener("keypress", addListAfterKeypress);
 
